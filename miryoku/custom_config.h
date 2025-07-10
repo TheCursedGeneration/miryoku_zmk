50b40c1cd0eb53206bb678f7460fe60e48dab43e
TheCursedGeneration <144630167+TheCursedGeneration@users.noreply.github.com>
2025-07-10 01:26:42 -0600

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,4 +1,13 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define ZMK_POINTING_DEFAULT_MOVE_VAL 1500
+#define ZMK_POINTING_DEFAULT_MOVE_VAL 1250
+#define ZMK_POINTING_DEFAULT_SCRL_VAL 65
+
+#define U_MOUSE_MOVE_EXPONENT 1
+#define U_MOUSE_MOVE_TIME 0
+#define U_MOUSE_MOVE_DELAY 0
+
+#define U_MOUSE_SCROLL_EXPONENT 1
+#define U_MOUSE_SCROLL_TIME 0
+#define U_MOUSE_SCROLL_DELAY 0
